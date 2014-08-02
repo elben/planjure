afa42a73cdb15d601b0f49677d34046eae9423b8
Elben Shira <elbenshira@gmail.com>
2014-08-02 18:40:30 -0500

    Update clojurescript and om.

--- project.clj
+++ project.clj
@@ -4,13 +4,13 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-2173"]
+                 [org.clojure/clojurescript "0.0-2277"]
                  [org.clojure/core.async "0.1.303.0-886421-alpha"]
-                 [om "0.6.2"]
+                 [om "0.7.0"]
                  [org.clojure/data.priority-map "0.0.5"]
                  [tailrecursion/cljs-priority-map "1.1.0"]]
 
-  :plugins [[lein-cljsbuild "1.0.2"]
+  :plugins [[lein-cljsbuild "1.0.3"]
             [com.cemerick/clojurescript.test "0.3.1"]
             [com.keminglabs/cljx "0.4.0"]]
 
